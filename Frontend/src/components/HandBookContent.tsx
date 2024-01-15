8c2db4991217224624715dd0df6b16d8bd45fdaa
aqsa-irfan <aqsa.irfan9399@gmail.com>
2024-01-15 21:04:56 +0500

    update HandBookContent , utils

--- Frontend/src/components/HandBookContent.tsx
+++ Frontend/src/components/HandBookContent.tsx
@@ -11,16 +11,35 @@ const HandBookContent = () => {
   const { id } = useParams();
   const content = hanbookContent.find((content) => content.path===id)
 
+ 
+
   return (
 
+
     <div style={cardStyle} className='w-2/3 ml-10 mt-10 p-10 mb-10'>
-    <h5 className='text-4xl mb-5'>{content?.title}</h5>
+    <h5 className='text-4xl mb-5 font-bold'>{content?.title}</h5>
     <p className='text-lg'>
-    {content?.content}
+   
+    {content?.content.desc}
     </p>
+    <h1 className='text-black text-3xl mt-2 '>{content?.content.h1}</h1>
+    <h2 className='text-black mt-4 text-2xl '>{content?.content.h1a}</h2>
+    
+    {content?.code.map((item , index)=>(
+      <>
+      <p className='text-black mt-4 text-xl'>{item.head}</p>
+       <div className='bg-neutral-200 text-black px-6 text-lg w-[70%] py-4 mt-2 shadow-md shadow-neutral-400 '><pre>{item.codeString}</pre></div>
+       <p className='mt-2 text-black'>{item.explanation}</p>
+       </>
+
+    ))}
+    
+   
+ 
 
     </div>
-  )
+  
+  );
 }
 
 export default HandBookContent
--- Frontend/src/utils.ts
+++ Frontend/src/utils.ts
@@ -51,12 +51,32 @@ export const links = [
         id: "1",
         title: "Variables",
         path:'variables',
-        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, dolor? Sunt voluptatibus iste obcaecati, possimus provident iusto porro a blanditiis repellat at velit quo eveniet deleniti itaque adipisci repudiandae et molestias animi laborum sed ullam. Quidem non dicta adipisci laudantium voluptates, impedit optio ipsam reiciendis mollitia, quisquam qui nesciunt distinctio quia exercitationem dolorum enim praesentium quibusdam, similique repudiandae hic vitae nam repellendus? Illum atque, possimus vel odio, eligendi vitae quos illo laborum, tempora error consequatur quibusdam molestiae id! Accusantium quam nisi necessitatibus debitis dolorem! Delectus, fugiat natus. Natus dolores, quis id ullam deserunt debitis asperiores vitae officia laborum explicabo minus cumque accusantium possimus laudantium non nostrum enim. Recusandae quasi rerum temporibus voluptatibus necessitatibus quae accusantium tempora modi, voluptates architecto at, magnam deserunt laborum id. Maxime harum aut sequi fugit dolores in accusantium tempora, vitae, ad assumenda beatae officia iste, voluptatibus doloribus dolor eligendi quo earum! Ut, vel? Odio excepturi dolore non nam, quis minus, labore totam alias possimus accusamus iusto eos accusantium ab illum at voluptas enim aliquid? Ut reiciendis aliquid quidem odit libero minima sit recusandae fugiat facere delectus et amet eum doloribus nobis, id provident voluptatum culpa? Velit aliquam harum distinctio nihil ratione aspernatur nobis reiciendis maiores, est sit, dolor pariatur tenetur, nesciunt iste earum incidunt sequi odit veniam! Quae nobis dolores perspiciatis dolore recusandae ullam nulla at, hic eveniet suscipit consequatur sequi? Sed nisi ipsum ipsa, soluta quisquam incidunt blanditiis libero tenetur, corporis corrupti molestias deserunt quidem sunt exercitationem laborum culpa magni voluptas distinctio tempore cupiditate possimus obcaecati! Impedit, repellat provident! Accusamus eveniet at provident libero exercitationem nisi vel nesciunt saepe temporibus, fuga sed nemo? Facere nobis porro eius fugit laboriosam, quisquam vel! Vel iure, ea eaque assumenda accusamus aspernatur sit animi accusantium, libero placeat quas totam nobis necessitatibus doloribus architecto nisi? Neque eos dolorum error explicabo, id unde ipsa corporis iusto vitae quisquam quia, aliquam officiis, dolorem magnam rem aperiam expedita hic veniam necessitatibus assumenda iure et. Amet totam voluptates, possimus at iste placeat non saepe quis quasi odio doloribus enim voluptate minima optio soluta. Nemo fugit beatae rerum totam consequatur, impedit repudiandae perspiciatis cupiditate temporibus ab, in excepturi porro cum! Veritatis autem, impedit suscipit laborum vero nulla nihil. Est aperiam, dignissimos iusto exercitationem pariatur animi incidunt numquam delectus quasi eum porro repellendus quas magnam. Similique harum maxime, possimus placeat deleniti voluptatem earum nulla dolorem adipisci unde omnis impedit quod? Sed exercitationem quo rerum error consequatur veritatis, facilis maxime at qui quia animi, deleniti nam cumque ea quasi possimus reiciendis debitis molestias a perferendis! Fugit in eos, eaque quam dolorem dicta porro! Molestiae, est fugit? Et totam laudantium voluptate magni voluptatum? Ipsam voluptatem molestias, fugit animi repellat expedita maiores repudiandae eius, doloribus velit, inventore in excepturi amet? Inventore ea cumque incidunt laborum? Sit debitis, incidunt cumque possimus asperiores quod reiciendis id. Itaque impedit, repudiandae rerum cumque, eius provident tempora natus ullam ratione delectus, maiores iste eaque omnis dolorem quaerat earum dolorum architecto voluptas id corrupti obcaecati consequuntur voluptatem? Odio similique labore ipsa tempore nesciunt ea voluptatem natus corrupti officiis illum! Ipsa saepe velit sit! Impedit asperiores, veritatis modi vero ratione consectetur? Nisi odit praesentium temporibus esse exercitationem vel numquam est tempora, tempore id neque. Nisi sed debitis ea nulla fugiat esse dolores, beatae quae consectetur, odit nihil, repellendus neque doloremque vero dolorum aspernatur magni quisquam sit temporibus cumque totam soluta? Incidunt, dignissimos quas. A est quas nulla sit officiis omnis qui quisquam tenetur, illum alias at placeat distinctio quos cupiditate voluptatum, similique optio! Nam voluptate dolores maxime, veritatis esse fugit incidunt, cupiditate quis omnis delectus impedit, quos ipsum tempora. Possimus a aspernatur provident, asperiores ducimus ab!",
+        content:{desc:"In Goatlang, variables serve as containers for storing and manipulating data. Let's unravel the basics of working with variables in this caprine-inspired language.",
+      h1:"Declaration"},
         code : [
             {
-                codeString:"",
-                explanation:""
+              head:"Local Scope",  
+              codeString:"a = 1",
+                explanation:"Here, the variable a is limited to the local scope, accessible within its defined context."
+            },
+            {
+              head:"Global Scope",
+              codeString:"global const a = 1;",
+              explanation:"For global scope, use the global keyword before declaring the variable.By prefixing with global, you extend the variable's scope to the entire program, making it accessible from anywhere"
+              
+            },
+            {
+              head:"Constants",
+              codeString:"const pi = 3.14; ",
+              explanation:"Declare constants using the const keyword. Constants cannot be changed once assigned."
+              
+            },{
+              head:"Data Types",
+              codeString:"var age = 3   //integer \nvar weight = 42.5  // float \nvar name = 'Billy' " ,
+              explanation:"Declare constants using the const keyword. Constants cannot be changed once assigned."
+              
             }
+            
         ]
     },
     {
