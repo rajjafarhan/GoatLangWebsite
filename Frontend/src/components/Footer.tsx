ab8d9a935d17df4c75d0a14656bd88bdc470de46
Usman Siddiqui <siddiquiusman328@gmail.com>
2024-01-16 03:44:26 +0500

    ...

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Cards from './Cards';
 
 const Footer = () => {
@@ -8,41 +9,40 @@ const Footer = () => {
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
               {/* Column 1 */}
               <div>
-                <h3 className="font-bold mb-2">GoatLang</h3>
+                <h1 className="font-bold mb-2">GoatLang</h1>
                 <p>Made with love in Karachi, Pakistan</p>
                     <p>&copy; 2024 GoatLang. All rights reserved. </p><p> Contact us: contact@goatlang.com</p>
               </div>
     
               {/* Column 2 */}
               <div>
-                <h3 className="font-bold mb-2">Dummy Links</h3>
+                <h3 className="font-bold mb-2">Download</h3>
                 <ul>
-                  <li><a href="#" className="underline">Link 1</a></li>
-                  <li><a href="#" className="underline">Link 2</a></li>
-                  <li><a href="#" className="underline">Link 3</a></li>
-                  <li><a href="#" className="underline">Link 4</a></li>
+                  <li><Link to="https://www.npmjs.com/package/goat-code" className="underline">NPM</Link></li>
+                  <li><Link to="https://github.com/rajjafarhan/GoatLangWebsite" className="underline">Github</Link></li>
+                  <li><Link to="https://www.npmjs.com/package/goat-code" className="underline">Installation</Link></li>
                 </ul>
               </div>
     
               {/* Column 3 */}
               <div>
-                <h3 className="font-bold mb-2">Dummy Links</h3>
+                <h3 className="font-bold mb-2">Learn</h3>
                 <ul>
-                  <li><a href="#" className="underline">Link 1</a></li>
-                  <li><a href="#" className="underline">Link 2</a></li>
-                  <li><a href="#" className="underline">Link 3</a></li>
-                  <li><a href="#" className="underline">Link 4</a></li>
+                  <li><Link to="/handbook/handbook-content/datatypes" className="underline">Data Types</Link></li>
+                  <li><Link to="/handbook/handbook-content/variables" className="underline">Variable</Link></li>
+                  <li><Link to="/handbook/handbook-content/function" className="underline">Functions</Link></li>
+                  <li><Link to="/handbook/handbook-content/ifelse" className="underline">Conditional Statements</Link></li>
                 </ul>
               </div>
     
               {/* Column 4 */}
               <div>
-                <h3 className="font-bold mb-2">Dummy Links</h3>
+                <h3 className="font-bold mb-2">Interact</h3>
                 <ul>
-                  <li><a href="#" className="underline">Link 1</a></li>
-                  <li><a href="#" className="underline">Link 2</a></li>
-                  <li><a href="#" className="underline">Link 3</a></li>
-                  <li><a href="#" className="underline">Link 4</a></li>
+                  <li><Link to="/playground" className="underline">Playground</Link></li>
+                  <li><Link to="/feedback" className="underline">FeedBack</Link></li>
+                  <li><Link to="/feedback" className="underline">Community</Link></li>
+                  <li><Link to="/feedback" className="underline">Contribute</Link></li>
                 </ul>
               </div>
             </div>
