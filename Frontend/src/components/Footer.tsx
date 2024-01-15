20648260bd367f1fce9b5b8717b0332d28076155
Usman Siddiqui <siddiquiusman328@gmail.com>
2024-01-16 03:26:50 +0500

    ...

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -4,17 +4,13 @@ import Cards from './Cards';
 const Footer = () => {
     return (
         <footer className="bg-teal-500 text-zinc-50 py-8">
-          <div className="container mx-auto">
-            {/* Custom Component */}
-            
-            {/* Horizontal Line */}
-    
-            {/* Four Columns */}
+          <div className="container mx-10 mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
               {/* Column 1 */}
               <div>
                 <h3 className="font-bold mb-2">GoatLang</h3>
                 <p>Made with love in Karachi, Pakistan</p>
+                    <p>&copy; 2024 GoatLang. All rights reserved. </p><p> Contact us: contact@goatlang.com</p>
               </div>
     
               {/* Column 2 */}
